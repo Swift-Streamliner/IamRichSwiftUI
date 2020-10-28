95e2dac7cb6c35b36f833dddece088307b4baff7
thisisstreamliner <thisisstreamliner@gmail.com>
2020-10-28 12:33:15 +0200

    Initial Commit

---
+++ I am Rich SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I am Rich SwiftUI
+//
+//  Created by Tiger Mei on 28.10.2020.
+//  Copyright © 2020 Tiger Mei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
